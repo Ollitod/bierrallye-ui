e9ba8835e8efb7d097a8719e7bbd399874174d55
Tod Oliver <olivertod11@yahoo.de>
2024-06-05 23:48:44 +0200

    Fix year and mail, use cards and fix exercise plan

--- apps/bierrallye-irnfritz/src/app/info/hardfacts/hardfacts.component.ts
+++ apps/bierrallye-irnfritz/src/app/info/hardfacts/hardfacts.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCard, MatCardContent } from '@angular/material/card';
 
 @Component({
   selector: 'app-hardfacts',
   standalone: true,
-  imports: [],
+  imports: [MatCard, MatCardContent],
   templateUrl: './hardfacts.component.html',
   styleUrls: ['./hardfacts.component.scss'],
 })
