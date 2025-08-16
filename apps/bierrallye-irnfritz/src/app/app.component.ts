014a91161f48f363b62df68a53912b8761c39ecf
Tod Oliver <olivertod11@yahoo.de>
2025-08-16 12:58:14 +0200

    feat: conditionally render header based on route

--- apps/bierrallye-irnfritz/src/app/app.component.ts
+++ apps/bierrallye-irnfritz/src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component, computed, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
 import { FooterComponent } from './core/footer/footer.component';
 import { HeaderComponent } from './core/header/header.component';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import {
   Role,
   TokenService,
   UserService,
 } from '@bierrallye/shared/data-access';
 import { MatIconRegistry } from '@angular/material/icon';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,6 +18,22 @@ import { MatIconRegistry } from '@angular/material/icon';
   imports: [HeaderComponent, FooterComponent, RouterOutlet],
 })
 export class AppComponent implements OnInit {
+  private router = inject(Router);
+
+  private _hiddenHeaderRoutes = ['/overview'];
+
+  showHeader = toSignal(
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map(
+        (event) =>
+          !this._hiddenHeaderRoutes.some((url) =>
+            event.urlAfterRedirects.endsWith(url)
+          )
+      )
+    )
+  );
+
   isUser = computed(() => this.userService.user()?.role === Role.USER);
 
   constructor(
